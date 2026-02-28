bb2df45ac1236a5e4933ddce6cf0e88f969b3000
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2026-02-28 16:06:39 +0530

    ...

--- Brute_Force/Array/Max.js
+++ Brute_Force/Array/Max.js
@@ -1,10 +1,10 @@
 function FindMaxInArray(arr){
     let max=arr[0];
-    for(let i=1;i<arr.length-1;i++){
+    for(let i=1;i<arr.length;i++){
         if(arr[i]>max){
             max=arr[i];
         }
     }
     return max;
 }
-console.log(FindMaxInArray([7,4,5,1,9]));
+console.log(FindMaxInArray([8,4,5,1,9]));
