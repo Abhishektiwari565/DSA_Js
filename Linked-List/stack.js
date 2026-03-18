ab5885419952b6e61f9369c1df21b9e7a8bb5fef
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2026-03-18 19:28:03 +0530

    .....

---
+++ Linked-List/stack.js
@@ -0,0 +1,36 @@
+class Node{
+    constructor(value){
+        this.data=value;
+        this.next=null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.top=null;
+    }
+    push(value){
+        let node=new Node(value);
+        if(!this.top){
+            this.top=node;
+            return
+        }       
+        node.next=this.top
+        this.top=node;  //1->2
+    }
+    pop(){
+        if(!this.top){
+            console.log("stack is empty")
+            return ;
+        }
+        this.top=this.top.next;
+    }
+}
+let stack=new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.pop();
+
+console.log(stack.top.data);
+console.log(stack.top.next.data);
