03d35a69439bc0fb475c4a4db897fca88f499c93
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2026-03-18 09:45:03 +0530

    ....

--- Linked-List/circular.js
+++ Linked-List/circular.js
@@ -13,10 +13,46 @@ class LinkedList{
         if(!this.head){
             this.head=node;
             this.head.next=this.head;
-            console.log(this.head.next);
+            return;
         }
+        let current=this.head;
+        while(current.next!==this.head){
+            current=current.next;
+        }
+        current.next=node;
+        node.next=this.head;
+    }
+    insertAtIndex(index,value){
+        let node =new Node(value);
+        if(!this.head){
+            this.head=node;
+            this.head.next=this.head;
+            return;
+        }
+        let current=this.head;
+        for(let i=0;i<index-1;i++){
+            current=current.next;
+        }
+        current.next=node;
+        node.next=current.next.next;
+    }
+    print(){
+        let current=this.head;
+        let result=' ';
+        while(current.next!==this.head){
+            result+=current.data+"->";
+            current=current.next;
+        }
+        result+=current.data;
+        console.log(result);
     }
 }
 
 let list=new LinkedList();
-list.pop(4);
+list.push(4);
+list.push(8);
+list.push(12);
+list.push(16);
+list.insertAtIndex(2,20);
+
+list.print();
