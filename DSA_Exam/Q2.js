99fcca9f18377aa62c8be1324a3d701cf3008bd6
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2026-05-22 16:01:16 +0530

    ..

--- DSA_Exam/Q2.js
+++ DSA_Exam/Q2.js
@@ -1,8 +1,24 @@
 function selectionSort(arr) {
-    let min=arr[0];
-    for(let i=1;i<arr.length;i++){
-        if(arr[i]<min){
-            min=arr[i];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+
+        let minIndex = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
         }
+
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    return arr;
 }
-}
+
+let arr = [64, 25, 12, 22, 11];
+
+console.log(selectionSort(arr));
